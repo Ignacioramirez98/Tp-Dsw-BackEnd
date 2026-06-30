277eb884e6342cfcd8e0f3aaa6bad981084e8a30
Milton <miltonsarkissian@hotmail.com>
2026-06-30 19:24:23 -0300

    fix key orm

--- mikro-orm.config.ts
+++ mikro-orm.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from '@mikro-orm/core';
 import { MongoDriver } from '@mikro-orm/mongodb';
 
+if (!process.env.MONGO_URI) {
+  throw new Error('MONGO_URI environment variable is not set. Please check your .env file.');
+}
+
 export default defineConfig({
   driver: MongoDriver,
-  clientUrl: process.env.MONGO_URI || 'mongodb+srv://nacho98nacho98:dsw123@cluster0.z5xdoug.mongodb.net/crm',
+  clientUrl: process.env.MONGO_URI,
   dbName: 'crm',
   entities: ['dist/**/*.entity.js'],
   entitiesTs: ['src/**/*.entity.ts'],
